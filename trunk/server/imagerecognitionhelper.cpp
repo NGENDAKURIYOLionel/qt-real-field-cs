cb01f907f07ea6e7e55ee1d64f9d91514726943c
rvuonnal@cc.hut.fi <rvuonnal@cc.hut.fi@0d914ff2-4590-fa9e-f416-568ff7510efc>
2011-03-17 12:14:35 +0000

    added server project and one class skeleton

---
+++ trunk/server/imagerecognitionhelper.cpp
@@ -0,0 +1,5 @@
+#include "imagerecognitionhelper.h"
+
+ImageRecognitionHelper::ImageRecognitionHelper()
+{
+}
---
+++ trunk/server/imagerecognitionhelper.h
@@ -0,0 +1,10 @@
+#ifndef IMAGERECOGNITIONHELPER_H
+#define IMAGERECOGNITIONHELPER_H
+
+class ImageRecognitionHelper
+{
+public:
+    ImageRecognitionHelper();
+};
+
+#endif // IMAGERECOGNITIONHELPER_H
