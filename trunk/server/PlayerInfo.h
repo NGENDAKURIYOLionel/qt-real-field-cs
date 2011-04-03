7a9fd578ade3d0a104d8cba6017055710882cb7f
karthik.mallavarapu@gmail.com <karthik.mallavarapu@gmail.com@0d914ff2-4590-fa9e-f416-568ff7510efc>
2011-04-03 23:26:09 +0000

    Player information header file for database helper class.

---
+++ trunk/server/PlayerInfo.h
@@ -0,0 +1,45 @@
+/*
+ * PlayerInfo.h
+ *
+ * Author: Karthik Mallavarapu
+ */
+
+#ifndef PLAYERINFO_H_
+#define PLAYERINFO_H_
+
+#include<string>
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<stdint.h>
+using namespace std;
+class PlayerInfo {
+public:
+	PlayerInfo(string,string);
+	virtual ~PlayerInfo();
+	int setScore(int);
+	string getUID();
+	string getPassword();
+	void addKill();
+	void addDeath();
+	int getScore();
+	int getDeaths();
+	int getKills();
+	bool addInvite(uint64_t);
+	bool removeInvite(uint64_t);
+	vector<uint64_t> getInvites();
+	void printInvite();
+
+private:
+	string UID;
+	string password;
+	int numberOfDeaths;
+	int numberOfKills;
+	int numberOfInvites;
+	uint64_t score;
+	vector<uint64_t> invites;
+
+
+};
+
+#endif /* PLAYERINFO_H_ */
