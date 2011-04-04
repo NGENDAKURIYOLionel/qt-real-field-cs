feb1f3edb09f60d10fa7f61f88aaca85804beabb
rvuonnal <rvuonnal@cc.hut.fi>
2011-04-04 04:50:29 +0000

    - added json library - imagerecognitionhelper internals implementation - imagerecognitionhelper public api still incomplete

--- server/imagerecognitionhelper.h
+++ server/imagerecognitionhelper.h
@@ -1,29 +1,50 @@
 #ifndef IMAGERECOGNITIONHELPER_H
 #define IMAGERECOGNITIONHELPER_H
-#include "curl/curl.h"
 #include <string>
 #include <vector>
+#include "curl/curl.h"
+#include "json/json.h"
 
 typedef unsigned game_id_t;
 typedef unsigned player_id_t;
 typedef std::string jpeg_image_t;
+typedef std::vector<jpeg_image_t> jpeg_images_t;
+typedef std::string tid_t;
+typedef std::string uid_t;
+typedef std::vector<uid_t> uids_t;
+typedef std::string namespace_t;
+typedef enum {
+	NOT_ENOUGH_PLAYERS,
+	CURL_ERROR,
+	JSON_ERROR,
+	MALFORMED_RESPONSE
+} errors_e;
 
 class ImageRecognitionHelper
 {
 public:
-	ImageRecognitionHelper();
+	ImageRecognitionHelper(namespace_t&);
 	~ImageRecognitionHelper();
-	void post(); // will become private
-	void post_multipart(); // will become private
 
 	// "the API"
-	game_id_t start_game(std::vector<jpeg_image_t>); // image index in vector = player_id
-	player_id_t match(jpeg_image_t, game_id_t);
+	game_id_t start_game(jpeg_images_t&); // image index in vector = player_id
+	player_id_t match(jpeg_image_t&, game_id_t);
 	void end_game(game_id_t);
 
 private:
-	CURL* easy_handle;
-//	void decode_node(std::string, std::vector<std::string>&);
+//	CURL* easy_handle;
+	// needs to match the one created manually at developer.face.com
+	namespace_t current_namespace;
+
+	void post(std::string&, std::string&, Json::Value&);
+	void post_multipart(curl_httppost*, std::string&, Json::Value&);
+	void account_limits();
+	void faces_detect(jpeg_image_t&, tid_t&);
+	void faces_recognize(uids_t&, jpeg_image_t);
+	void faces_train(uid_t&);
+	void tags_remove(tid_t&);
+	void tags_save(tid_t&, uid_t&);
+	game_id_t games;
 };
 
 #endif // IMAGERECOGNITIONHELPER_H
--- server/main.cpp
+++ server/main.cpp
@@ -1,8 +1,45 @@
 #include "imagerecognitionhelper.h"
-//#include <iostream>
+#include <iostream>
+#include <vector>
+//#include <fstream>
+#include <cstdio>
+#include <ctime>
+#define BUFSIZE 4096
+#define NAMESPACE_NAME "T1105130_2"
 
 int main(int argc, char *argv[])
 {
-	ImageRecognitionHelper irh;
-	irh.post();
+	clock_t start = clock();
+	FILE* file1 = fopen("front.jpg", "rb");
+	if (file1 == NULL) throw;
+	FILE* file2 = fopen("diagonal.jpg", "rb");
+	if (file2 == NULL) throw;
+	jpeg_image_t player1;
+	unsigned char buf[BUFSIZE];
+	while(1) {
+		size_t read = fread(buf, 1, BUFSIZE, file1);
+		player1.append((const char*)buf, read);
+		if (feof(file1)) break;
+	}
+	fclose(file1);
+	jpeg_image_t player2;
+	while(1) {
+		size_t read = fread(buf, 1, BUFSIZE, file2);
+		player2.append((const char*)buf, read);
+		if (feof(file2)) break;
+	}
+	fclose(file2);
+	namespace_t current_namespace(NAMESPACE_NAME);
+	ImageRecognitionHelper irh(current_namespace);
+	jpeg_images_t players;
+	players.push_back(player1);
+	players.push_back(player2);
+//	irh.account_limits();
+	irh.start_game(players);
+	clock_t end = clock();
+	std::cout
+			<< "whole program: "
+			<< (end-start)/(CLOCKS_PER_SEC/1000)
+			<< " ms"
+			<< std::endl;
 }
