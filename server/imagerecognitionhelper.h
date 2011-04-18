ef2699293be64d2fc65c4535d3b3333e61968927
rvuonnal <rvuonnal@cc.hut.fi>
2011-04-18 06:43:52 +0000

    - new api for ImageRecognitionHelper - testing program for ImageRecognitionHelper

--- server/imagerecognitionhelper.h
+++ server/imagerecognitionhelper.h
@@ -7,12 +7,6 @@
 
 typedef unsigned game_id_t;
 typedef unsigned player_id_t;
-typedef std::string jpeg_image_t;
-typedef std::vector<jpeg_image_t> jpeg_images_t;
-typedef std::string tid_t;
-typedef std::string uid_t;
-typedef std::vector<uid_t> uids_t;
-typedef std::string namespace_t;
 typedef enum {
 	NOT_ENOUGH_PLAYERS,
 	CURL_ERROR,
@@ -23,31 +17,32 @@ typedef enum {
 class ImageRecognitionHelper
 {
 public:
-	ImageRecognitionHelper(namespace_t&);
+	ImageRecognitionHelper(std::string&);
 	~ImageRecognitionHelper();
 
 	// "the API"
-	void register_player(uid_t&, jpeg_image_t&);
-	void match(uid_t& response, jpeg_image_t&, uids_t&);
+	void register_player(std::string& uid, std::string& jpeg_image);
+	void match(std::string& response, std::string& jpeg_image, std::vector<std::string>& uids);
+	void account_users(std::vector<std::string>& response);
 
 	// deprecated
-	game_id_t start_game(jpeg_images_t&); // image index in vector = player_id
-	player_id_t match(jpeg_image_t&, game_id_t);
+	game_id_t start_game(std::vector<std::string>&); // image index in vector = player_id
+	player_id_t match(std::string&, game_id_t);
 	void end_game(game_id_t);
 
 private:
 //	CURL* easy_handle;
 	// needs to match the one created manually at developer.face.com
-	namespace_t current_namespace;
+	std::string current_namespace;
 
-	void post(std::string&, std::string&, Json::Value&);
-	void post_multipart(curl_httppost*, std::string&, Json::Value&);
+	void post(std::string& post_data, std::string& post_url, Json::Value& decoded_response);
+	void post_multipart(curl_httppost* post_data, std::string& post_url, Json::Value& decoded_response);
 	void account_limits();
-	void faces_detect(jpeg_image_t&, tid_t&);
-	void faces_recognize(uids_t&, jpeg_image_t);
-	void faces_train(uid_t&);
-	void tags_remove(tid_t&);
-	void tags_save(tid_t&, uid_t&);
+	void faces_detect(std::string& jpeg_image, std::string& tid_response);
+	void faces_recognize(std::vector<std::string>& uids, std::string jpeg_image, std::string& response);
+	void faces_train(std::string&);
+	void tags_remove(std::string&);
+	void tags_save(std::string&, std::string&);
 	game_id_t games;
 };
 
--- server/main.cpp
+++ server/main.cpp
@@ -6,9 +6,16 @@
 #include <ctime>
 #define NAMESPACE_NAME "20110415"
 
-void read_file(const char* src, jpeg_image_t& target) {
+std::string current_namespace(NAMESPACE_NAME);
+ImageRecognitionHelper irh(current_namespace);
+
+typedef enum {
+	FILE_NOT_FOUND
+} main_errors;
+
+void read_file(const char* src, std::string& target) {
 	FILE* f = fopen(src, "rb");
-	if (f == NULL) throw;
+	if (f == NULL) throw FILE_NOT_FOUND;
 #define BUFSIZE 4096
 	unsigned char buf[BUFSIZE];
 	while(1) {
@@ -19,31 +26,82 @@ void read_file(const char* src, jpeg_image_t& target) {
 	fclose(f);
 }
 
+void register_test(std::string uid, std::string picture) {
+	clock_t start = clock();
+	std::string jpeg_picture_data;
+	try {
+		read_file(picture.c_str(), jpeg_picture_data);
+	} catch(...) {
+		std::cout << "error reading image" << std::endl;
+		return;
+	}
+	irh.register_player(uid, jpeg_picture_data);
+	clock_t end = clock();
+	std::cout << "register: "
+			  << (end-start)/(CLOCKS_PER_SEC/1000)
+			  << " ms"
+			  << std::endl;
+}
+
+void users_test() {
+	clock_t start = clock();
+	std::vector<std::string> r;
+	irh.account_users(r);
+	clock_t end = clock();
+	std::cout << "register: "
+			  << (end-start)/(CLOCKS_PER_SEC/1000)
+			  << " ms"
+			  << std::endl;
+}
+
+void match_test() {
+
+}
+
 int main(int argc, char *argv[])
 {
-	clock_t start = clock();
-	jpeg_image_t user1_reg;
-	jpeg_image_t user1_detect;
-	jpeg_image_t user2_reg;
-	jpeg_image_t user2_detect;
+	if (argc > 1) {
+		std::string argv1(argv[1]);
+		if (!argv1.compare("register")) {
+			std::cout << "registering user" << std::endl;
+			if (argc != 4) {
+				std::cout << "usage: register uid face.jpg" << std::endl;
+				return 0;
+			}
+			std::string uid(argv[2]);
+			std::string picture(argv[3]);
+			register_test(uid, picture);
+			return 0;
+		} else if (!argv1.compare("match")) {
+			std::cout << "matching user" << std::endl;
+			return 0;
+		} else if (!argv1.compare("users")) {
+			users_test();
+			return 0;
+		}
+	}
+	std::cout << "valid command line arguments: register, match" << std::endl;
+	return 0;
+
+	std::vector<std::string> users_response;
+	irh.account_users(users_response);
+	std::string user1_reg;
+	std::string user1_detect;
+	std::string user2_reg;
+	std::string user2_detect;
 	read_file("user1_reg.jpg", user1_reg);
 	read_file("user1_detect.jpg", user1_detect);
 	read_file("user2_reg.jpg", user2_reg);
 	read_file("user2_detect.jpg", user2_detect);
-	namespace_t current_namespace(NAMESPACE_NAME);
-	ImageRecognitionHelper irh(current_namespace);
-	jpeg_images_t players;
+	std::vector<std::string> players;
 	players.push_back(user1_reg);
 	players.push_back(user2_reg);
-	uid_t user1("asdf");
-	uid_t user2("qwe");
-	irh.register_player(user1);
-	irh.register_player(user2);
+	std::string user1("asdf");
+	std::string user2("qwe");
+//	irh.register_player(user1, user1_reg);
+//	irh.register_player(user2, user2_reg);
+//	std::string response;
+//	irh.match(response, user1_detect, user1, user2);
 //	irh.account_limits();
 //	irh.start_game(players);
-	clock_t end = clock();
-	std::cout << "whole program: "
-			  << (end-start)/(CLOCKS_PER_SEC/1000)
-			  << " ms"
-			  << std::endl;
 }
