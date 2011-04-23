6ece0404e852e5c33ff1a69161d453d5198f570e
rvuonnal@cc.hut.fi <rvuonnal@cc.hut.fi@0d914ff2-4590-fa9e-f416-568ff7510efc>
2011-04-23 11:46:35 +0000

    replaced qimage with qbytearray

--- trunk/server/player.h
+++ trunk/server/player.h
@@ -2,9 +2,9 @@
 #define PLAYER_H
 
 #include <QObject>
-#include <QImage>
 #include <QDate>
 #include <QHash>
+#include <QByteArray>
 #define _PLAYER_KILL_DAMAGE 100
 
 class Player : public QObject
@@ -19,7 +19,7 @@ signals:
     void startGameSignal();
     void endGameSignal();
     void abortGameSignal();
-    void shotSignal(QImage* image, QString* player);
+    void shotSignal(QByteArray* image, QString* player);
     void joinTeamSignal(QString* player, QString* team);
     void leaveGameSignal(QString* player);
     //MessageHandler side
@@ -38,7 +38,7 @@ signals:
 
 public slots:
     void loginWithPassword(QString* uname,QString* password);
-    void loginWithPicture(QString* uname,QImage* picture);
+    void loginWithPicture(QString* uname,QByteArray* picture);
     void logout(QString* uname);
     void createGame(QString* uname, QString* game_id, int duration);
     void setTime(QString* game_id, QDate* date);
@@ -48,7 +48,7 @@ public slots:
     void joinTeam(QString* uname,QString* team_id);
     void leave(QString* uname);
     void cancel(QString* uname);
-    void shoot(QString* uname,QImage* picture);
+    void shoot(QString* uname,QByteArray* picture);
     void gameStart(QString* uname);
     void gameStarted();
     void gameEnded(QString *win_team, QList<Player*> *players);
