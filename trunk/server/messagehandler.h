d87d3a65ee12d84eff2387b515ea6cffa3736f39
lewang0418 <lewang0418@0d914ff2-4590-fa9e-f416-568ff7510efc>
2011-04-23 13:55:01 +0000

    update messagehandler.cpp/h according to ../message/message.png

--- trunk/server/messagehandler.h
+++ trunk/server/messagehandler.h
@@ -18,22 +18,27 @@ public slots:
     void sendMessageSlot(QString message);
 
 signals:
+    //Caution:
+    //Pass pointers to slot functions may crash the program if slot functions uses these pointers after returning back
+    //to the functions which emit these singals
     void loginWithPassword(QString* uname,QString* password);
     void loginWithPicture(QString* uname,QByteArray* picture);
     void logout(QString* uname);
     void gamelist(QString* uname);
-    void createGame(QString* uname, QString* gameId, int duration);
-    void setTime(QString* uname,QDate* date);
-    void setDuration(QString* uname,int duration);
-    void setPlayers(QString* uname,int players);
-    void setTeams(QString* uname,int teams);
-    void invite(QString* uname,QString* targetName,int gameId);
+    void createGame(QString* uname, QString* gameId, int duration, int noOfTeamA, int noOfTeamB);
     void joinGame(QString* uname,QString* gameId);
     void joinTeam(QString* uname,QString* teamId);
     void leave(QString* uname);
-    void cancel(QString* uname);
     void shoot(QString* uname,QByteArray* picture);
     void gameStart(QString* uname);
+
+      //uncessary signals according to ../message/message.png
+//    void setTime(QString* uname,QDate* date);
+//    void setDuration(QString* uname,int duration);
+//    void setPlayers(QString* uname,int players);
+//    void setTeams(QString* uname,int teams);
+//    void invite(QString* uname,QString* targetName,int gameId);
+//    void cancel(QString* uname);
 
 private:
     QTcpSocket *tcpsocket;
