36f6cc92f33639b5687dbf79cc2a701a59849e66
karthik.mallavarapu@gmail.com <karthik.mallavarapu@gmail.com@0d914ff2-4590-fa9e-f416-568ff7510efc>
2011-05-06 13:02:01 +0000

    connected game with server object

--- trunk/server/gamefactory.h
+++ trunk/server/gamefactory.h
@@ -5,13 +5,16 @@
 #include <QList>
 #include <QHash>
 #include "game.h"
+#include "server.h"
 
 class game;
+class Server;
 namespace GameFactory{
     bool exists(QString id);
     game* getGame(QString id);
     QList<QString> getGameIds();
     void destroyGame(QString id);
+    void setServer(Server *s);
 }
 
 #endif // GAMEFACTORY_H
